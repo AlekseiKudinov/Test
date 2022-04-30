9ddb3762c7ddc06a35b6083b1980855a5dedd466
AlekseiKudinov <machette94@gmail.com>
2022-04-30 16:21:53 +0300

    Create FirstViewController.

---
+++ Test/FirstViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.swift
+//  Test
+//
+//  Created by Алексей Кудинов on 30.04.2022.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+}
